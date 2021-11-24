1e8f78c7662149db132050831747574f39cf2032
Sno3t <tycho.brakenhoff@ictmbo.nl>
2021-11-24 23:55:27 +0100

    Added blocks and ps files

--- src/main/java/com/snoet/plushie/block/Modblocks.java
+++ src/main/java/com/snoet/plushie/block/Modblocks.java
@@ -6,6 +6,8 @@ import com.snoet.plushie.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,6 +20,8 @@ public class Modblocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Plushie.MOD_ID);
 
+    public static final RegistryObject<Block> STUFFING_BLOCK = registerBlock("stuffing_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(2f)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
